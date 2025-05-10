f6e830d84a4a6a2a272b22089824ad9eeb53518d
ARUN-KR07 <arunkrkr12@gmail.com>
2025-05-10 18:01:46 +0530

    Updated portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,7 +7,7 @@ def home(request):
     json_path = os.path.join(settings.BASE_DIR, 'portfolio', 'projects.json')
     with open(json_path, 'r') as f:
         projects = json.load(f)
-    return render(request, 'portfolio/home.html', {'projects': projects})
+    return render(request, 'portfolio/index.html', {'projects': projects})
 
 def new_page_view(request):
     return render(request, 'bio.html')
